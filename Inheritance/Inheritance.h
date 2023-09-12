2d46bf7da51ac46ec2705c454ef76899f137fb71
79141 <79141@DESKTOP-GMGD17F>
2023-09-12 22:11:32 +0900

    DynamicCastDone

---
+++ Inheritance/Inheritance.h
@@ -0,0 +1,86 @@
+#pragma once
+#include<iostream>
+#include<string>
+using namespace std;
+
+#define delimiter  "\n///-----------------------------------------------------------------------------------------------------///\n"
+
+#define HUMAN_TAKE_PARAMETERS const std::string& last_name, const std::string& first_name, int age
+#define HUMAN_GIVE_PARAMETERS last_name, first_name, age
+class Human {
+	std::string last_name;
+	std::string first_name;
+	int age;
+public:
+	const std::string& get_last_name()const;
+	const std::string& get_first_name()const;
+	int get_age()const;
+	void set_last_name(const std::string& last_name);
+	void set_first_name(const std::string& first_name);
+	void set_age(int age);
+
+	//					Constructors:
+	Human(HUMAN_TAKE_PARAMETERS);
+	virtual ~Human();
+
+	virtual void print()const;
+};
+std::ostream& operator<<(std::ostream& os, const Human& obj);
+
+#define STUDENT_TAKE_PARAMETERS	const std::string& speciality, const std::string& group, double rating, double attendance
+#define STUDENT_GIVE_PARAMETERS	speciality, group, rating, attendance
+class Student :public Human
+{
+	std::string speciality;
+	std::string group;
+	double rating;
+	double attendance;
+public:
+	const std::string& get_speciality()const;
+	const std::string& get_group()const;
+	double get_rating()const;
+	double get_attendance()const;
+	void set_speciality(const std::string& speciality);
+	void set_group(const std::string& group);
+	void set_rating(double rating);
+	void set_attendance(double attendance);
+
+	//					Constructors:
+	Student(HUMAN_TAKE_PARAMETERS, STUDENT_TAKE_PARAMETERS);
+	~Student();
+
+	void print()const;
+};
+std::ostream& operator<<(std::ostream& os, const Student& obj);
+
+#define TEACHER_TAKE_PARAMETERS const std::string& speciality, int experience
+#define TEACHER_GIVE_PARAMETERS speciality, experience
+class Teacher :public Human
+{
+	std::string speciality;
+	int experience;
+public:
+	const std::string& get_speciality()const;
+	int get_experience()const;
+	void set_speciality(const std::string& speciality);
+	void set_experience(int experience);
+	Teacher(HUMAN_TAKE_PARAMETERS, TEACHER_TAKE_PARAMETERS);
+
+	void print()const;
+};
+std::ostream& operator<<(std::ostream& os, const Teacher& obj);
+
+class Graduate :public Student
+{
+	std::string subject;
+public:
+	const std::string& get_subject()const;
+	void set_subject(const std::string& subject);
+
+	Graduate(HUMAN_TAKE_PARAMETERS, STUDENT_TAKE_PARAMETERS, const std::string& subject);
+	~Graduate();
+	void print()const;
+};
+
+std::ostream& operator<<(std::ostream& os, const Graduate& obj);
+
--- Inheritance/main.cpp
+++ Inheritance/main.cpp
@@ -1,172 +1,49 @@
 #include <iostream>
+#include <fstream>
+#include "Inheritance.h"
+
 using namespace std;
 
-#define HUMAN_TAKE_PARAMETERS const std::string& last_name, const std::string& first_name, int age
-#define HUMAN_GIVE_PARAMETERS last_name, first_name, age 
-class Human{
-    std::string last_name;
-	std::string first_name;
-	int age;
-public:
-	const std::string& get_last_name()const {
-		return last_name;
-	}
-	const std::string& get_first_name()const {
-		return first_name;
-	}
-	int get_age()const {
-		return age;
-	}
-	void set_last_name(const std::string& last_name) {
-		this->last_name = last_name;
-	}
-	void set_first_name(const std::string& first_name) {
-		this->first_name = first_name;
-	}
-	void set_age(int age) {
-		this->age = age;
-	}
+#define delimiter  "\n///-----------------------------------------------------------------------------------------------------///\n"
 
-	//            Constructors:
+void print(Human* group[], const int n);
+void save(Human* group[], const int n, const char sz_filemane[]);
 
-	Human(HUMAN_TAKE_PARAMETERS) {
-		set_last_name(last_name);
-		set_first_name(first_name);
-		set_age(age);
-		cout << "HConstructor:\t" << this << endl;
-	}
-	~Human() {
-		cout << "HDestructor:\t" << this << endl;
-	}
+//#define INHERITANCE_CHECK
 
-	virtual void print()const {
-		cout << last_name << " " << first_name << " " << age << " y.o." << endl;
-	}
-};
-
-#define STUDENT_TAKE_PARAMETERS const std::string& speciality, const std::string group, double rating, double attendance
-#define STUDENT_GIVE_PARAMETERS speciality, group, rating, attendance
-class Student :public Human {
-	std::string speciality;
-	std::string group;
-	double rating;
-	double attendance;
-public:
-	const std::string& get_speciality()const {
-		return speciality;
-	}
-	const std::string& get_group()const {
-		return group;
-	}
-	double get_rating()const {
-		return rating;
-	}
-	double get_attendance()const {
-		return attendance;
-	}
-	void set_speciality(const std::string& speciality) {
-		this->speciality = speciality;
-	}
-	void set_group(const std::string& group) {
-		this->group = group;
-	}
-	void set_rating(double rating){
-		this->rating = rating;
-	}
-	void set_attendance(double attendance) {
-		this->attendance = attendance;
-	}
-
-	//            Constructors:
-	Student(HUMAN_TAKE_PARAMETERS,STUDENT_TAKE_PARAMETERS) :Human(HUMAN_GIVE_PARAMETERS)
-	{
-		set_speciality(speciality);
-		set_group(group);
-		set_rating(rating);
-		set_attendance(attendance);
-		cout << "SConstructor:\t" << this << endl;
-	}
-	~Student() {
-		cout << "SDestructor:\t" << this << endl;
-	}
-	
-
-	void print()const {
-		Human::print();
-		cout << speciality << " " << group << " " << rating << " " << attendance << endl;
-	}
-};
-
-#define TEACHER_TAKE_PARAMETERS const std::string& speciality, int experience
-class Teacher :public Human {
-std::string speciality;
-int experience;
-public:
-	const std::string& get_speciality()const {
-		return speciality;
-	}
-	int get_experience()const {
-		return experience;
-	}
-	void set_speciality(const std::string& speciality) {
-		this->speciality= speciality;
-	}
-	void set_experience(int experience) {
-		this->experience = experience;
-	}
-	Teacher(HUMAN_TAKE_PARAMETERS, TEACHER_TAKE_PARAMETERS) :Human(HUMAN_GIVE_PARAMETERS) {
-		
-
-	}
-	~Teacher() {
-	
-	}
-	void print() const{
-		Human::print();
-		cout << speciality << " " << experience << " years\n";
-	}
-};
-
-class Graduate :public Student {
-	std::string subject;
-public:
-	const std::string& get_subject()const {
-		return subject;
-	}
-	void set_subject(const std::string& subject) {
-		this->subject = subject;
-	}
-
-	Graduate(HUMAN_TAKE_PARAMETERS, STUDENT_TAKE_PARAMETERS, const std::string& subject)
-	:Student(HUMAN_GIVE_PARAMETERS, STUDENT_GIVE_PARAMETERS)
-	{
-		set_subject(subject);
-		cout << "GConstructor:\t" << this << endl;
-	}
-	~Graduate() {
-		cout << "GDestructor:\t" << this << endl;
-	}
-};
-void main() {
+void main()
+{
 	setlocale(LC_ALL, "");
-	Human human("Montana", "Antonio", 25);
+#ifdef INHERITANCE_CHECK
+	Human human("Montana", "Antomio", 25);
 	human.print();
 
 	Student student("Pinkman", "Jessie", 22, "Chemistry", "WW_220", 90, 95);
 	student.print();
 
-	Teacher teacher("White", "Walter", 50, "Chemisty", 20);
+	Teacher teacher("White", "Walter", 50, "Chemistry", 20);
 	teacher.print();
 
-	Graduate graduate("Schrader", "Hank", 40, "Criminalistic", "OBN", 80, 70, "How to catch Heisengern");
+	Graduate graduate("Schrader", "Hank", 40, "Criminalistic", "OBN", 80, 70, "How to catch Heisenberg");
 	graduate.print();
+#endif // INHERITANCE_CHECK
 
-	Human* group[] = {
-	new Student("Pinkman", "Jessie", 22, "Chemistry", "WW_220", 90, 95),
-	new Teacher("White", "Walter", 50, "Chemisty", 20),
-	new Graduate("Schrader", "Hank", 40, "Criminalistic", "OBN", 80, 70, "How to catch Heisengern")
+	Human* group[] =
+	{
+		new Student("Pinkman", "Jessie", 22, "Chemistry", "WW_220", 90, 95),
+		new Teacher("White", "Walter", 50, "Chemistry", 20),
+		new Graduate("Schrader", "Hank", 40, "Criminalistic", "OBN", 80, 70, "How to catch Heisenberg")
 	};
+	print(group, sizeof(group) / sizeof(group[0]));
+	
 	for (int i = 0; i < sizeof(group) / sizeof(group[0]); i++) {
-		group[i]->print();
+		delete group[i];
 	}
+
+}
+
+void save(Human* group[], const int n, const char sz_filemane[]) {
+	std::ofstream fout(sz_filemane);
+
+	fout.close();
 }
