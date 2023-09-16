fd87d2e983f1a98a6a4e32a430b170de16efceaa
Mousence <93631072+Mousence@users.noreply.github.com>
2023-09-16 19:43:54 +0900

    main.cpp

--- Inheritance/main.cpp
+++ Inheritance/main.cpp
@@ -52,58 +52,6 @@ void save(Human* group[], const int n, const char sz_filename[]) {
 	system(command.c_str());
 }
 
-/*void load(Human* C_group[], const int n, const char sz_filename[]) {
-	std::ifstream fin(sz_filename);
-	if (fin.is_open()) {
-		std::string line;
-
-		std::string last_name;
-		std::string first_name;
-		int age;
-
-		//std::string speciality;
-		int experience;
-		std::string subject;
-
-		int i = 0;
-		while (std::getline(fin, line)) {
-			std::string Class;
-			std::string Type;
-			std::istringstream iss(line);
-			iss >> Class >> Type;
-		   if (Type == "Student") {
-			std::string speciality;
-			std::string group;
-			double rating;
-			double attendance;
-			iss >> Class >> Type >> HUMAN_GET_PARAMETERS >> STUDENT_GET_PARAMETERS;
-			C_group[i] = new Student(HUMAN_GIVE_PARAMETERS, STUDENT_GIVE_PARAMETERS);
-			}
-		   else if (Type == "Teacher") {
-			std::string speciality;
-			int experience;
-			iss >> Class >> Type >> HUMAN_GET_PARAMETERS >> TEACHER_GET_PARAMETERS;
-			C_group[i] = new Teacher(HUMAN_GIVE_PARAMETERS, TEACHER_GIVE_PARAMETERS);
-		    }
-		   else if (Type == "Graduate") {
-			   std::string speciality;
-			   std::string group;
-			   double rating;
-			   double attendance;
-			   std::string subject;
-			   iss >> Class >> Type >> HUMAN_GET_PARAMETERS >> STUDENT_GET_PARAMETERS >> subject;
-			   C_group[i] = new Graduate(HUMAN_GIVE_PARAMETERS, STUDENT_GIVE_PARAMETERS, subject);
-		   }
-		   i++;
-		}
-	}
-	else {
-		std::cerr << "Error: File doesnt exist!" << endl;
-	}
-	fin.close();
-}
-*/
-
 void load(Human* C_group[], const int n, const char sz_filename[]) {
 	std::ifstream fin(sz_filename);
 	if (fin.is_open()) {
